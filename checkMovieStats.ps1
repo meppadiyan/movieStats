744cfaf7f901e2e97bba481cd3fbc669bb8fd2ff
meppadiyan <123039568+meppadiyan@users.noreply.github.com>
2023-02-21 02:31:18 +1100

    Update checkMovieStats.ps1

--- checkMovieStats.ps1
+++ checkMovieStats.ps1
@@ -380,7 +380,7 @@ Class MovieStats {
             }
 }
 
-$date = "2023-02-20"
+$date = "2023-02-21"
 
 $nanpakalMovieStats = [MovieStats]::new("Nanpakal","Nanpakal","",@('19','66','58'),@(),$date)
 $nanpakalMovieStats.Init()
